09b05ada8fd25658776fa540946c83a8c47557ef
Daniil Shelepin <@shelepin.daniil>
2025-10-24 17:40:05 +0300

    add tests logs

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,4 +1,5 @@
 import requests
+from utils.logger import Logger
 """Список Http Методов"""
 class HttpMethods:
     headers = {
@@ -9,20 +10,28 @@ class HttpMethods:
     
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+        Logger.add_response(result)
         return result
     
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body,  headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+        Logger.add_response(result)
         return result
     
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body,  headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+        Logger.add_response(result)
         return result
     
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body,  headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+        Logger.add_response(result)
         return result
